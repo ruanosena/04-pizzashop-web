581430d11e8f239374a04ae5ffc244041b5215aa
ruanosena <ruosena@hotmail.com>
2024-02-04 10:44:19 -0300

    tests end to end

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('20', { exact: true })).toBeVisible()
+  await expect(page.getByText('+7% em relação a ontem')).toBeVisible()
+})
+
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('200', { exact: true })).toBeVisible()
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^200-5% em relação ao mês passado$/ })
+      .getByRole('paragraph'),
+  ).toBeVisible()
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('5', { exact: true })).toBeVisible()
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^5-5% em relação ao mês passado$/ })
+      .getByRole('paragraph'),
+  ).toBeVisible()
+})
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(
+    page.locator('span').filter({ hasText: 'R$ 20,00' }),
+  ).toBeVisible()
+  await expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
+})
